44f15a4f9fb580bd56c13ac6a896bc66a82d1158
amclen <andrew.mclenigan@gmail.com>
2021-04-20 20:36:06 -0400

    implement search algorithm

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+from search import search
 
 def main():
     parser = argparse.ArgumentParser(add_help=True)
@@ -18,10 +19,6 @@ def main():
 
     args = parser.parse_args()
     search(args.input_path, args.output_path)
-
-def search(input_path, output_path):
-    print(input_path)
-    print(output_path)
 
 
 if __name__ == '__main__':
---
+++ src/search.py
@@ -0,0 +1,50 @@
+def search(input_path, output_path):
+    text_file = open(input_path, 'r')
+    lines = text_file.readlines()
+    text_file.close()
+
+    words = lines[0].strip().split(',')
+    grid = []
+    for line in lines[1:]:
+        grid.append(line.strip().split(','))
+
+    max_row = len(grid)
+    max_col = len(grid[0])
+    for word in words:
+        for r in range(max_row):
+            for c in range(max_col):
+                if grid[r][c] == word[0]:
+                    coords = find_word(r, c, max_row, max_col, word, grid)
+                    if coords:
+                        print(coords)
+
+
+def find_word(r, c, max_row, max_col, word, grid):
+    directions = [(1,0),(1,1),(0,1),(0,-1),(-1,-1),(-1,0),(1,-1),(-1,1)]
+    
+    for x, y in directions:
+        res = [(c, r)]
+        curr=r+x
+        curc=c+y
+
+        curres = res
+        for i in range(1, len(word)):
+            if curr>=max_row or curr<0 or curc>=max_col or curc<0:
+                break
+
+            if grid[curr][curc] != word[i]:
+                break
+
+            curres.append((curc, curr))
+            if len(curres)==len(word):
+                return curres
+            curr+=x
+            curc+=y
+
+            
+
+
+
+
+
+    
